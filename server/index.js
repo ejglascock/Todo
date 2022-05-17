105623c1b360ebcb6c96d4a95eb396ac04e4a3aa
Elijah Glascock <ejglascock@gmail.com>
2022-05-17 12:26:28 -0700

    added get id, update, and delete routes

--- server/index.js
+++ server/index.js
@@ -14,12 +14,12 @@ app.use(express.json());
 app.post('/todos', async(req,res) => {
     try {
         const {description} = req.body;
-        const newToDo = await pool.query(
+        const newTodo = await pool.query(
             "INSERT INTO todo (description) VALUES($1) RETURNING *", 
             [description]
         );
 
-        res.json(newToDo.rows[0]);
+        res.json(newTodo.rows[0]);
     } catch (error) {
         console.error(error.message);
     }
@@ -29,8 +29,8 @@ app.post('/todos', async(req,res) => {
 
 app.get("/todos", async(req, res) => {
     try {
-        const allToDos = await pool.query("SELECT * FROM todo");
-        res.json(allToDos.rows);
+        const allTodos = await pool.query("SELECT * FROM todo");
+        res.json(allTodos.rows);
     } catch (error) {
         console.error(error.message);
     };
@@ -38,9 +38,47 @@ app.get("/todos", async(req, res) => {
 
 //get a todo
 
+app.get("/todos/:id", async(req,res) => {
+    try {
+        const {id} = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+
+        res.json(todo.rows[0]);
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
 //update a todo
 
+app.put("/todos/:id", async(req, res) => {
+    try {
+       const {id} = req.params;
+       const {description} = req.body;
+       const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
+       [description, id]
+       );
+
+       res.json("Todo was updated");
+    } catch (error) {
+        console.error(error.message);
+    };
+});
+
 //delete a todo
+
+app.delete("/todos/:id", async(req, res) => {
+    try {
+       const {id} = req.params;
+       const deletTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+
+       res.json("Todo was deleted")
+    } catch (error) {
+        console.error(error.message);
+    }
+})
+
+// ROUTES end //
 
 app.listen(5000, () => {
     console.log('server has started on port 5000');
